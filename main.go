016c92599abf457d89faaf82b0c0e1bfc16f4d60
ante-neh <amareanteneh12@gmail.com>
2024-06-10 14:35:25 +0300

    feat(backend):complete the functionality of validate chirpy service

--- main.go
+++ main.go
@@ -14,20 +14,18 @@ func main() {
 
 	//pattern [METHOD] [HOST]/[PATH] Note that all three parts are optional.
 
-
-
 	//route
 	mux.Handle("GET api/healthz", server.logMiddleware(http.HandlerFunc(server.handleHealthz)))
 	mux.Handle("GET api/metrics", server.logMiddleware(http.HandlerFunc(server.handleMetrics)))
 	mux.Handle("/reset", server.logMiddleware(http.HandlerFunc(server.handleReset)))
 	mux.Handle("/app/static/", server.logMiddleware(server.metricsMiddleware(http.HandlerFunc(server.handleStatic))))
 	mux.Handle("/app/images/", server.logMiddleware(server.metricsMiddleware(http.HandlerFunc(server.handleImages))))
-
+	mux.Handle("POST /app/validate_chirpy", server.logMiddleware(http.HandlerFunc(server.handleValidateChirpy)))
 
 
 	fmt.Printf("Server running on port %s \n", port)
 
 	if err := http.ListenAndServe(port, mux); err != nil{
-		fmt.Println("Server is not working")
+		fmt.Println(err)
 	}
 }
