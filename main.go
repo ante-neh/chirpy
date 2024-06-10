6383e6be0d9bae11882f09342f32ce8e85099554
ante-neh <amareanteneh12@gmail.com>
2024-06-10 15:02:42 +0300

    update(backend):use respondWithJson to handle response for handleMetrics, and handleReset services

--- main.go
+++ main.go
@@ -13,18 +13,15 @@ func main() {
 	port := ":5000"
 
 	//pattern [METHOD] [HOST]/[PATH] Note that all three parts are optional.
-
 	//route
-	mux.Handle("GET api/healthz", server.logMiddleware(http.HandlerFunc(server.handleHealthz)))
-	mux.Handle("GET api/metrics", server.logMiddleware(http.HandlerFunc(server.handleMetrics)))
+	mux.Handle("GET /api/healthz", server.logMiddleware(http.HandlerFunc(server.handleHealthz)))
+	mux.Handle("GET /api/metrics", server.logMiddleware(http.HandlerFunc(server.handleMetrics)))
 	mux.Handle("/reset", server.logMiddleware(http.HandlerFunc(server.handleReset)))
 	mux.Handle("/app/static/", server.logMiddleware(server.metricsMiddleware(http.HandlerFunc(server.handleStatic))))
 	mux.Handle("/app/images/", server.logMiddleware(server.metricsMiddleware(http.HandlerFunc(server.handleImages))))
-	mux.Handle("POST /app/validate_chirpy", server.logMiddleware(http.HandlerFunc(server.handleValidateChirpy)))
-
+	mux.Handle("POST /app/validate_chirpy", server.logMiddleware(server.metricsMiddleware(http.HandlerFunc(server.handleValidateChirpy))))
 
 	fmt.Printf("Server running on port %s \n", port)
-
 	if err := http.ListenAndServe(port, mux); err != nil{
 		fmt.Println(err)
 	}
